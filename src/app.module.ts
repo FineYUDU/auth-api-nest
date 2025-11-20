8e989b214026652a7b9400ef02a53f401ce6ba0a
FineYudu <fine_567@hotmail.com>
2025-11-20 15:00:09 -0600

    app.modules.ts:ConfigModule and TypeOrmModule configuration width environments and db in docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type:'postgres',
+      username:process.env.DB_USERNAME,
+      password:process.env.DB_PASSWORD,
+      host:process.env.DB_HOST,
+      database:process.env.DB_NAME,
+      port:+(process.env.DB_PORT || 8080),
+      autoLoadEntities:true,
+      // TODO: Remove synchronize in production 
+      synchronize:true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
