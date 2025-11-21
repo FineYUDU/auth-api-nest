57b9180c6e58786bec2121b78605e7a24e7c1ca4
FineYudu <fine_567@hotmail.com>
2025-11-20 19:10:15 -0600

    create user in auth service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,13 +3,14 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 import { CreateUserDto, LoginUserDto } from './dto';
+import { ResponseRegisterUser } from './interfaces';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  create(@Body() createUserDto: CreateUserDto) {
+  create(@Body() createUserDto: CreateUserDto):Promise<ResponseRegisterUser | undefined> {
     return this.authService.create( createUserDto );
   }
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import * as bcrypt from 'bcrypt';
+import { Repository } from 'typeorm';
+
+import { User } from './entities/user.entity';
+
 import { CreateUserDto, LoginUserDto } from './dto';
+
+import { ResponseRegisterUser } from './interfaces';
 
 @Injectable()
 export class AuthService {
 
-  create(createUserDto: CreateUserDto) {
-    return 'This action create user';
-  }
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+  ) {} 
+
+  async create(createUserDto: CreateUserDto):Promise<ResponseRegisterUser | undefined> {
+
+    try {
+      const { password, email, firstName, lastName } = createUserDto;
+
+      const user = this.userRepository.create({
+        email,
+        firstName,
+        lastName,
+        password:bcrypt.hashSync(password, 10)
+      });
+
+      await this.userRepository.save(user);
+      const createResponse = await {
+        email:user.email,
+        firstName:user.firstName,
+        lastName:user.lastName,
+      }
+
+      return createResponse;
+      
+    } catch (error) {
+      this.handleDbError(error)
+    }
+  };
+
+
   login(loginUserDto: LoginUserDto) {
     return 'This action login user';
-  }
+  };
+
+
+  private handleDbError(error:any):void {
+    if(error.code === '23505') throw new BadRequestException(error.detail);
+
+    console.log(error);
+
+    throw new InternalServerErrorException('Please check server logs');
+  };
 
 }
